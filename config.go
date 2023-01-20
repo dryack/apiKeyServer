0efc0bf3e32220c3ff54f67b94a1b2e18e9199e8
dryack <dave.ryack@gmail.com>
2023-01-20 14:23:00 -0800

    intermediate. starting OnConfigChange() handling

--- config.go
+++ config.go
@@ -27,7 +27,21 @@ func doConfiguration() error {
 	}
 	viper.OnConfigChange(func(e fsnotify.Event) {
 		//TODO: will need to be sending to logfile
-		fmt.Println("Config file changed: ", e.Name)
+
+		// fmt.Println("Config file changed: ", e.Name)
+		tempKeys := Keys{
+			TotalExhaustions: keys.TotalExhaustions,
+			TotalKeysServed:  keys.TotalKeysServed,
+			ServerVersion:    keys.ServerVersion,
+			StartupTime:      keys.StartupTime,
+		}
+		if err := viper.Unmarshal(&tempKeys); err != nil {
+			fmt.Println("fucking fuck")
+			Log.Warn().Msg("new configuration failed to unmarshal")
+		} else {
+			Log.Info().Msg("new configuration loaded")
+			liveUpdateConfiguration(&tempKeys)
+		}
 	})
 	viper.WatchConfig()
 	err := viper.Unmarshal(&keys)
@@ -37,4 +51,31 @@ func doConfiguration() error {
 		return err
 	}
 	return err
+}
+
+func liveUpdateConfiguration(tempKeys *Keys) {
+	for _, nKey := range tempKeys.Apikeys {
+		for _, oKey := range keys.Apikeys {
+			if nKey.User == oKey.User {
+				if int32(nKey.MaxPerMinute) < oKey.CurrentlyRemaining {
+					nKey.CurrentlyRemaining = int32(nKey.MaxPerMinute)
+				} else {
+					nKey.CurrentlyRemaining = oKey.CurrentlyRemaining
+				}
+				nKey.Active = oKey.Active
+				nKey.Tornkey = oKey.Tornkey
+				nKey.ReturnToService = oKey.ReturnToService
+				nKey.Kills = oKey.Kills
+				nKey.Uses = oKey.Uses
+			} else {
+				nKey.CurrentlyRemaining = int32(nKey.MaxPerMinute)
+			}
+
+		}
+	}
+	for _, key := range tempKeys.Apikeys {
+		tempKeys.TotalPerMinute += key.MaxPerMinute
+	}
+	fmt.Println(tempKeys)
+	// newConf := viper.AllSettings()
 }
